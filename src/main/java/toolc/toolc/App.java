f31a760591b50c186faab9220a01ccb192de178e
neodiogenes <alteraa@yahoo.com>
2021-03-26 17:03:24 -0400

    Initial commit

---
+++ src/main/java/toolc/toolc/App.java
@@ -0,0 +1,13 @@
+package toolc.toolc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
